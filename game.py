39fa4f6d73892578a0671a6c5c0c43c66d5f7eb5
Joshua Carney <joshuakcarney@gmail.com>
2022-02-11 14:36:38 -0500

    i will have tkinter's head

--- game.py
+++ game.py
@@ -5,6 +5,8 @@ from map import Map
 from item import Item
 from player import Player, RandomPlayer
 from map_factory import OutdoorMapFactory
+import tkinter as tk
+import visual
 
 
 class FireEmblem:
@@ -19,81 +21,86 @@ class FireEmblem:
         self.loss_condition_encountered = False
         self.win_condition_encountered = False
 
+    def player_phase(self):
+        for unit in self.allies:
+            new_coords = self.ally_controller.determine_move_coordinates(self.allies, self.enemies, self.map, unit)
+            unit.goto(new_coords[0], new_coords[1])
+            action_choice = self.ally_controller.determine_action(self.enemies, self.map, unit, unit.x, unit.y)
+            print(f"{unit.name} moved to coordinates {unit.x}, {unit.y} and chose {action_choice.name}")
+            if action_choice.is_attack():
+                # Defender in this case is from self.enemies
+                defender = action_choice.action_item
+                combat_stats = combat.get_combat_stats(unit, defender, self.map)
+                result = combat.simulate_combat(combat_stats)
+                print(f"\t {result.name}")
+
+                if result is CombatResults.DEFENDER_DEATH:
+                    self.enemies.remove(defender)
+                    if not self.enemies:
+                        self.win_condition_encountered = True
+
+                elif result is CombatResults.ATTACKER_DEATH:
+                    if unit.terminal_condition:
+                        self.loss_condition_encountered = True
+                    self.allies.remove(unit)
+
+            elif action_choice.is_item():
+                item_to_use = action_choice.action_item
+                heal_amount = item_to_use.info['heal_amount']
+                unit.heal(heal_amount)
+                item_to_use.info['uses'] -= 1
+                if item_to_use.info['uses'] == 0:
+                    unit.inventory.remove(item_to_use)
+
+    def enemy_phase(self):
+        for unit in self.enemies:
+            new_coords = self.enemy_controller.determine_move_coordinates(self.enemies, self.allies, self.map, unit)
+            unit.goto(new_coords[0], new_coords[1])
+            action_choice = self.enemy_controller.determine_action(self.allies, self.map, unit, unit.x, unit.y)
+            print(f"{unit.name} moved to coordinates {unit.x}, {unit.y} and chose {action_choice.name}")
+            if action_choice.is_attack():
+                # Defender in this case is from self.allies
+                defender = action_choice.action_item
+                combat_stats = combat.get_combat_stats(unit, defender, self.map)
+                result = combat.simulate_combat(combat_stats)
+                print(f"\t {result.name}")
+
+                if result is CombatResults.DEFENDER_DEATH:
+                    if defender.terminal_condition:
+                        self.loss_condition_encountered = True
+                    self.allies.remove(defender)
+
+                elif result is CombatResults.ATTACKER_DEATH:
+                    self.enemies.remove(unit)
+                    if not self.enemies:
+                        self.win_condition_encountered = True
+
+            elif action_choice.is_item():
+                item_to_use = action_choice.action_item
+                heal_amount = item_to_use.info['heal_amount']
+                unit.heal(heal_amount)
+                item_to_use.info['uses'] -= 1
+                if item_to_use.info['uses'] == 0:
+                    unit.inventory.remove(item_to_use)
+
+            if self.win_condition_encountered:
+                return 1
+            elif self.loss_condition_encountered:
+                return -1
+
     def run(self):
         while not self.loss_condition_encountered and not self.win_condition_encountered:
             self.turn_count += 1
             print(f"Turn: {self.turn_count}")
-            # Ally unit phase
-            for unit in self.allies:
-                new_coords = self.ally_controller.determine_move_coordinates(self.allies, self.enemies, self.map, unit)
-                unit.goto(new_coords[0], new_coords[1])
-                action_choice = self.ally_controller.determine_action(self.enemies, self.map, unit, unit.x, unit.y)
-                print(f"{unit.name} moved to coordinates {unit.x}, {unit.y} and chose {action_choice.name}")
-                if action_choice.is_attack():
-                    # Defender in this case is from self.enemies
-                    defender = action_choice.action_item
-                    combat_stats = combat.get_combat_stats(unit, defender, self.map)
-                    result = combat.simulate_combat(combat_stats)
-                    print(f"\t {result.name}")
 
-                    if result is CombatResults.DEFENDER_DEATH:
-                        self.enemies.remove(defender)
-                        if not self.enemies:
-                            self.win_condition_encountered = True
+            self.player_phase()
 
-                    elif result is CombatResults.ATTACKER_DEATH:
-                        if unit.terminal_condition:
-                            self.loss_condition_encountered = True
-                        self.allies.remove(unit)
+            if self.win_condition_encountered:
+                return 1
+            elif self.loss_condition_encountered:
+                return -1
 
-                elif action_choice.is_item():
-                    item_to_use = action_choice.action_item
-                    heal_amount = item_to_use.info['heal_amount']
-                    unit.heal(heal_amount)
-                    item_to_use.info['uses'] -= 1
-                    if item_to_use.info['uses'] == 0:
-                        unit.inventory.remove(item_to_use)
-
-                if self.win_condition_encountered:
-                    return 1
-                elif self.loss_condition_encountered:
-                    return -1
-
-            # Enemy movement phase
-            for unit in self.enemies:
-                new_coords = self.enemy_controller.determine_move_coordinates(self.enemies, self.allies, self.map, unit)
-                unit.goto(new_coords[0], new_coords[1])
-                action_choice = self.enemy_controller.determine_action(self.allies, self.map, unit, unit.x, unit.y)
-                print(f"{unit.name} moved to coordinates {unit.x}, {unit.y} and chose {action_choice.name}")
-                if action_choice.is_attack():
-                    # Defender in this case is from self.allies
-                    defender = action_choice.action_item
-                    combat_stats = combat.get_combat_stats(unit, defender, self.map)
-                    result = combat.simulate_combat(combat_stats)
-                    print(f"\t {result.name}")
-
-                    if result is CombatResults.DEFENDER_DEATH:
-                        if defender.terminal_condition:
-                            self.loss_condition_encountered = True
-                        self.allies.remove(defender)
-
-                    elif result is CombatResults.ATTACKER_DEATH:
-                        self.enemies.remove(unit)
-                        if not self.enemies:
-                            self.win_condition_encountered = True
-
-                elif action_choice.is_item():
-                    item_to_use = action_choice.action_item
-                    heal_amount = item_to_use.info['heal_amount']
-                    unit.heal(heal_amount)
-                    item_to_use.info['uses'] -= 1
-                    if item_to_use.info['uses'] == 0:
-                        unit.inventory.remove(item_to_use)
-
-                if self.win_condition_encountered:
-                    return 1
-                elif self.loss_condition_encountered:
-                    return -1
+            self.enemy_phase()
 
             if self.turn_count == 100:
                 self.loss_condition_encountered = True
@@ -105,8 +112,9 @@ class FireEmblem:
 
 
 if __name__ == "__main__":
+
     map_factory = OutdoorMapFactory(10, 15, 10, 15)
-    tile_map = map_factory.generate_map()[0]
+    tile_map,number_tile_map = map_factory.generate_map()
 
     lyn = Unit(0xceb4, 0, 0, 2, 0x0204, 17, 6, 8, 10, 6, 2, 0, 0, True, [0x1, 0x6b], True)
     bandit = Unit(0xe9b8, 0, 1, 2, 0x1410, 21, 4, 1, 4, 0, 3, 0, 0, False, [0x1f], False)
@@ -115,6 +123,7 @@ if __name__ == "__main__":
     enemies = [bandit]
 
     game = FireEmblem(tile_map, allies, enemies, RandomPlayer(), RandomPlayer())
+    viz = visual.BoardVisualization(tile_map.x, tile_map.y, number_tile_map, game)
     result = game.run()
 
     print(f"Here's the result of the game: {result}")
--- visual.py
+++ visual.py
@@ -1,39 +1,57 @@
 import tkinter as tk
 import map_factory
+import unit
 
 
 class BoardVisualization(tk.Tk):
-    def __init__(self, x_tiles, y_tiles, tile_map):
+    def __init__(self, x_tiles, y_tiles, tile_map, fe_game):
         tk.Tk.__init__(self)
         self.tile_map = tile_map
         self.colors = ['green2', 'blue', 'forest green', 'sienna4']
-
-
-        self.canvas = tk.Canvas(self, width=500, height=500, borderwidth=0, highlightthickness=0)
+        self.canvas = tk.Canvas(self, width=800, height=800, borderwidth=0, highlightthickness=0)
         self.canvas.pack(side="top", fill="both", expand="true")
         self.rows = x_tiles
         self.columns = y_tiles
         self.tiles = {}
-        self.canvas.bind("<Configure>", self.redraw)
+        self.ovals = {}
         self.status = tk.Label(self, anchor="w")
         self.status.pack(side="bottom", fill="x")
 
-    def redraw(self, event=None):
+        self.game = fe_game
+
+    def draw_units(self, event=None):
         self.canvas.delete("rect")
-        cellwidth = int(self.canvas.winfo_width()/self.columns)
-        cellheight = int(self.canvas.winfo_height()/self.columns)
+        self.canvas.delete("blueteam")
+        self.canvas.delete("redteam")
+        cellwidth = int(self.canvas.winfo_width()/self.columns/1.2)
+        cellheight = int(self.canvas.winfo_height()/self.columns/1.2)
         for column in range(self.columns):
             for row in range(self.rows):
-                x1 = column*cellwidth
+                x1 = column * cellwidth
                 y1 = row * cellheight
                 x2 = x1 + cellwidth
                 y2 = y1 + cellheight
                 tile = self.canvas.create_rectangle(x1,y1,x2,y2, fill=self.colors[int(self.tile_map[row][column])], tags="rect")
-                self.tiles[row,column] = tile
+                self.tiles[row, column] = tile
 
+        for blue_unit in self.game.allies:
+            x, y = blue_unit.x, blue_unit.y
+            x1 = x * cellwidth
+            x2 = x1 + cellwidth
+            y1 = y * cellheight
+            y2 = y1 + cellheight
+            oval = self.canvas.create_oval(x1 + 5, y1 + 5, x2 - 5, y2 - 5, outline="navy", fill="cyan", tags="blueteam")
+            self.ovals[x, y] = oval
 
-if __name__ == "__main__":
-    map_factory = map_factory.OutdoorMapFactory(10, 15, 10, 15)
-    tile_map, number_tile_map = map_factory.generate_map()
-    app = BoardVisualization(tile_map.x, tile_map.y, number_tile_map)
-    app.mainloop()
+        for red_unit in self.game.enemies:
+            x, y = red_unit.x, red_unit.y
+            x1 = x * cellwidth
+            x2 = x1 + cellwidth
+            y1 = y * cellheight
+            y2 = y1 + cellheight
+            oval = self.canvas.create_oval(x1 + 5, y1 + 5, x2 - 5, y2 - 5, outline="maroon", fill="red", tags="redteam")
+            self.ovals[x, y] = oval
+
+        if self.game.loss_condition_encountered or self.game.win_condition_encountered:
+            self.after(15, self.draw_units)
+
