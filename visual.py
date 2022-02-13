38274baa018d08cd0ddc8cd138502597638e4559
Joshua Carney <48502124+jkcarney@users.noreply.github.com>
2022-02-12 19:10:31 -0500

    visualization, yay

--- visual.py
+++ visual.py
@@ -12,13 +12,13 @@ class BoardVisualization(tk.Tk):
         tk.Tk.__init__(self)
 
         self.do_loop = tk.IntVar()
-        self.delay_value = tk.IntVar()
+        self.delay_value = tk.StringVar()
 
         self.advance = tk.Button(self, text="Advance", command=self.advance_button_callback)
         self.advance.pack()
         self.loop_check = tk.Checkbutton(self, text='Loop', variable=self.do_loop)
         self.loop_check.pack()
-        self.delay = tk.Spinbox(self, from_=0, to=5, textvariable=self.delay_value)
+        self.delay = tk.Spinbox(self, from_=0, to=5, increment=0.1, format='%.2f', textvariable=self.delay_value)
         self.delay.pack()
 
         self.game = game
@@ -28,22 +28,28 @@ class BoardVisualization(tk.Tk):
         self.canvas = tk.Canvas(self, width=600, height=600, borderwidth=0, highlightthickness=0)
         self.canvas.pack(side="top", fill="both", expand="true")
         self.canvas.bind("<Configure>", self.redraw)
-        self.rows = x_tiles
-        self.columns = y_tiles
+        self.rows = y_tiles
+        self.columns = x_tiles
         self.tiles = {}
         self.ovals = {}
 
     def advance_button_callback(self):
-        if self.do_loop == 1:
+        if self.do_loop.get() == 1:
             step_result = 0
             while step_result == 0:
                 print(f'TURN {self.game.turn_count}')
                 step_result = self.game.step()
                 self.redraw()
+                self.update()
+                time.sleep(float(self.delay_value.get()))
         else:
             print(f'TURN {self.game.turn_count}')
             step_result = self.game.step()
             self.redraw()
+
+        if step_result != 0:
+            print(f'Game is over: {step_result}')
+            self.advance.configure(state=tk.DISABLED)
 
     def redraw(self, event=None):
         self.canvas.delete("rect")
@@ -80,9 +86,9 @@ class BoardVisualization(tk.Tk):
 
 
 if __name__ == "__main__":
-    map_factory = OutdoorMapFactory(15, 15, 15, 15)
+    map_factory = OutdoorMapFactory(10, 15, 10, 15)
     tile_map,number_tile_map = map_factory.generate_map()
-    print(tile_map)
+
     lyn = Unit(0xceb4, 0, 0, 2, 0x0204, 17, 6, 8, 10, 6, 2, 0, 0, True, [0x1, 0x6b], True)
     bandit = Unit(0xe9b8, 0, 1, 2, 0x1410, 21, 4, 1, 4, 0, 3, 0, 0, False, [0x1f], False)
 
